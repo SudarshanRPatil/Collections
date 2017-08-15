d543a3d1fc02fef079ad78e3e3d8a6d7bf1eca22
SudarshanRPatil <mailtosudarshan7891@gmail.com>
2017-08-15 22:31:51 +0530

    initial commit

---
+++ CollectionsConsoleApp/CollectionsConsoleApp/MyDict.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionsConsoleApp
+{
+    public class MyDict<TK, TV>: IEnumerable<KeyValuePair<TK,TV>>
+    {
+        private const int IntialSize = 20;
+
+        private LinkedList<KeyValuePair<TK, TV>>[] _valuePairs;
+
+        public MyDict()
+        {
+            this._valuePairs = new LinkedList<KeyValuePair<TK, TV>>[IntialSize];
+        }
+
+        public int Count { get; private set; }
+
+        public int Capacity
+        {
+            get { return this._valuePairs.Length; }
+        }
+
+        public void Add(TK key, TV value)
+        {
+            var hash = this.HashKey(key);
+            if (this._valuePairs[hash] == null)
+            {
+                this._valuePairs[hash] = new LinkedList<KeyValuePair<TK, TV>>();
+            }
+
+            var keyAlreadyExists = this._valuePairs[hash].Any(x => x.Key.Equals(key));
+
+            if (keyAlreadyExists)
+            {
+              throw new InvalidOperationException("The key already exists, can not add two elements with same key.");
+            }
+
+            var keyValuePair = new KeyValuePair<TK,TV>(key, value);
+            this._valuePairs[hash].AddLast(keyValuePair);
+            this.Count++;
+
+            if (this.Count >= this.Capacity * 0.75)
+            {
+                this.ResizeAndReAddValues();
+            }
+        }
+
+        public TV Find(TK key)
+        {
+            var hash = key.GetHashCode();
+            if (this._valuePairs[hash] == null)
+            {
+                return default(TV);
+            }
+            var valuePair = this._valuePairs[hash];
+            return valuePair.First(x => x.Key.Equals(key)).Value;
+        }
+
+        public bool ContainsKey(TK key)
+        {
+            var hash = key.GetHashCode();
+            if (this._valuePairs[hash] == null)
+            {
+                return false;
+            }
+            var valuePair = this._valuePairs[hash];
+            return valuePair.Any(x => x.Key.Equals(key));
+        }
+
+        private int HashKey(TK Key)
+        {
+           return Math.Abs(Key.GetHashCode()) % this.Capacity;
+        }
+
+        private void ResizeAndReAddValues()
+        {
+            //cache the old values
+            var cacheValuePairs = this._valuePairs;
+            //resize
+            this._valuePairs = new LinkedList<KeyValuePair<TK, TV>>[2 * this.Capacity];
+            //Add values
+            this.Count = 0;
+            foreach (var cacheValuePair in cacheValuePairs)
+            {
+                if (cacheValuePair != null)
+                {
+                    foreach (var valuePair in cacheValuePair)
+                    {
+                        this.Add(valuePair.Key, valuePair.Value);
+                    }
+                }
+            }
+        }
+
+        public IEnumerator<KeyValuePair<TK, TV>> GetEnumerator()
+        {
+            foreach (var valuePair in this._valuePairs)
+            {
+                if (valuePair != null)
+                {
+                    foreach (var value in valuePair)
+                    {
+                        yield return value;
+                    }
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
---
+++ CollectionsConsoleApp/CollectionsConsoleApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionsConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dict = new MyDict<int, string>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                dict.Add(i+1, string.Format("Sud {0}", i+1));
+            }
+
+            foreach (var valuePair in dict)
+            {
+               Console.WriteLine("Key {0} and Value {1}",valuePair.Key, valuePair.Value);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
